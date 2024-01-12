1af10b44b32bd6f7603fa8382280163e3f6b5272
deceser <aufhaummtobsc@gmail.com>
2024-01-12 14:31:21 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import HelloWorld from "./components/HelloWorld.vue";
+  import DefaultLayout from "./layout/DefaultLayout.vue";
+</script>
+
+<template>
+  <DefaultLayout />
+  <HelloWorld msg="Dimitrova Card" />
+</template>
+
+<style scoped></style>
